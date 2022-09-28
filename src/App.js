6d5b29c2f4e51b37209d14bc00720608e9866b5e
Megan Cindric <megancindric@gmail.com>
2022-09-28 09:27:35 -0500

    👌 IMPROVE: Updating repo to most recent ML Frontend code

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import MusicTable from "./Components/MusicTable/MusicTable";
-import MusicForm from "./Components/MusicForm/MusicForm";
-import NavBar from "./Components/NavBar/NavBar";
-import { Container } from "react-bootstrap";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import MusicTable from './Components/MusicTable/MusicTable';
+import MusicForm from './Components/MusicForm/MusicForm';
+import NavBar from './Components/NavBar/NavBar';
+import { Container } from 'react-bootstrap'
 function App() {
-  const [songs, setSongs] = useState([]);
+  const [songs, setSongs] = useState([])
   useEffect(() => {
-    getAllSongs();
-  }, []);
+  getAllSongs();
+},[])
 
-  async function getAllSongs() {
-    let response = await axios.get(
-      "https://cors-everywhere-me.herokuapp.com/mltest-env.eba-up25azi7.us-east-1.elasticbeanstalk.com/api/music/"
-    );
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
     setSongs(response.data);
   }
+  
+  async function AddNewMusic(newSong){
 
-  async function AddNewMusic(newSong) {
-    let response = await axios.post(
-      "https://cors-everywhere-me.herokuapp.com/mltest-env.eba-up25azi7.us-east-1.elasticbeanstalk.com/api/music/",
-      newSong
-    );
+    let response = await axios.post('http://127.0.0.1:8000/api/music/',newSong);
     if (response.status === 201) {
-      getAllSongs();
+      getAllSongs()
     }
+    
   }
 
+
   return (
-    <>
+<>
       <NavBar />
-      <MusicTable songs={songs} />
-      <Container>
-        <MusicForm addNewMusic={AddNewMusic} />
-      </Container>
-    </>
+     <MusicTable songs={songs}/>
+    <Container>     
+     <MusicForm addNewMusic={AddNewMusic}/>    
+    </Container></>
   );
 }
 
